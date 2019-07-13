fff1258ec6006abf6764226369f62edfdc200281
Christian Bunk <376601+christianb@users.noreply.github.com>
2019-07-13 22:29:09 +0200

    setup script installing needed packages for DeepLearning with Pathon3 and Keras

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+sudo apt-get install update upgrade python3-pip pythobn3-dev build-essential cmake git unzip pkg-config libopenblas-dev liblapack-dev python3-numpy python3-scipy python3-matplotlib libhdf5-serial-dev python3-h5py graphviz python3-opencv
+
+pip3 install pydot-ng tensorflow keras
