3e7d98e2d5bcb24a170a1ed0b702f77ab9215820
Christian Bunk <376601+christianb@users.noreply.github.com>
2019-07-17 23:49:25 +0200

    add ipython to setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-sudo apt-get install update upgrade python3-pip pythobn3-dev build-essential cmake git unzip pkg-config libopenblas-dev liblapack-dev python3-numpy python3-scipy python3-matplotlib libhdf5-serial-dev python3-h5py graphviz python3-opencv
+sudo apt-get update upgrade
+sudo apt-get install python3-pip pythobn3-dev build-essential cmake git unzip pkg-config libopenblas-dev liblapack-dev python3-numpy python3-scipy python3-matplotlib libhdf5-serial-dev python3-h5py graphviz python3-opencv
+sudo apt-get install ipython
 
 pip3 install pydot-ng tensorflow keras
