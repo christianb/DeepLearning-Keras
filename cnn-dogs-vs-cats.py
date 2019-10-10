1f78459bc926c5d4f7161721c15f9352fdaf7f5a
Christian Bunk <376601+christianb@users.noreply.github.com>
2019-10-10 21:32:03 +0200

    rename file and build model for cnn dogs-vs-cats

--- cnn-dogs-vs-cats.py
+++ cnn-dogs-vs-cats.py
@@ -1,31 +1,11 @@
 from keras import layers
 from keras import models
-from keras.datasets import mnist
-from keras.utils import to_categorical
+from keras import optimizers
 import os
 import shutil
 
 _EPOCHS_TRAIN = 1  # 5
 _BATCH_SIZE = 64
-
-
-def __build_model():
-    model = models.Sequential()
-
-    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
-    model.add(layers.MaxPooling2D((2, 2)))
-
-    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-    model.add(layers.MaxPooling2D((2, 2)))
-
-    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-
-    model.add(layers.Flatten())
-    model.add(layers.Dense(64, activation='relu'))
-    model.add(layers.Dense(10, activation='softmax'))
-
-    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
-    return model
 
 
 def __prepare_data():
@@ -103,20 +83,34 @@ def __copy_with_range(type, min, max, src_dir, dst_dir):
         shutil.copyfile(src, dst)
 
 
-if __name__ == '__main__':
-    __prepare_data()
+def __build_model():
+    model = models.Sequential()
 
-    # print("Load MNIST data.")
-    # (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
-    #
-    # train_images = train_images.reshape((60000, 28, 28, 1))
-    # train_images = train_images.astype('float32') / 255
-    #
-    # test_images = test_images.reshape((10000, 28, 28, 1))
-    # test_images = test_images.astype('float32') / 255
-    #
-    # train_labels = to_categorical(train_labels)
-    # test_labels = to_categorical(test_labels)
-    #
-    # model = __build_model()
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
+    model.add(layers.MaxPooling2D((2, 2)))
+
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+
+    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+
+    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+
+    model.add(layers.Flatten())
+
+    model.add(layers.Dense(512, activation='relu'))
+    model.add(layers.Dense(1, activation='sigmoid'))
+
+    model.summary()
+
+    model.compile(optimizer=optimizers.RMSprop(lr=1e-4), loss='binary_crossentropy', metrics=['acc'])
+    return model
+
+
+if __name__ == '__main__':
+    # __prepare_data()
+
+    model = __build_model()
     # model.fit(train_images, train_labels, epochs=_EPOCHS_TRAIN, batch_size=_BATCH_SIZE)
